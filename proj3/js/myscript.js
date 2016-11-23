c36357ff19bbc2289c6d8a4895985af586df7ec8
lucyxwang06 <lucyxwang06@gmail.com>
2016-11-22 18:53:17 -0800

    !

---
+++ proj3/js/myscript.js
@@ -0,0 +1,20 @@
+$(function() {
+
+       $topIndex = 0;
+
+       $( ".ui-draggable" ).draggable().css({"position":"absolute","top":"0px" });;
+
+        $(".ui-draggable").click(function(){
+            $topIndex=$topIndex+1;
+            $(this).css("z-index",$topIndex);
+        });
+
+   });
+
+
+        $(function(){
+          $.okvideo({ source: '',
+                    volume: 0})
+        });
+
+
